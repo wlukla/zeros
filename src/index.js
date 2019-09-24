4b4e54d857860f5e165f62db97e124cdf40cfc5d
wlukla <vladislav.danilochkin@gmail.com>
2019-09-24 20:29:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function zeros(expression) {
-  // your solution
+
+  let arr = expression.split('*');
+  let countFive = 0;
+  let countTwo = 0;
+
+  arr.forEach(element => {
+
+    let match = element.match(/!/g).length;
+    element = parseInt(element);
+
+    for (element; element > 0; element -= match) {
+
+      // counting 5s
+      let t = element;
+      while (t % 5 == 0) {
+        countFive++;
+        t /= 5;
+      }
+
+      //counting 2s
+      t = element;
+      while (t % 2 == 0) {
+        countTwo++;
+        t /= 2;
+      }
+    }
+  });
+  // is 2s enough?
+  return countFive > countTwo ? countTwo : countFive;
 }
